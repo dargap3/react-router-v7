b441676f5fa33d181970f7216d3bad396e65592b
Diego Ramon <dargap@hotmail.com>
2025-05-07 15:00:59 -0500

    Router Library: Basic components desing using v0

---
+++ router-library/support-chat-library/src/App.tsx
@@ -0,0 +1,16 @@
+import { AuthLayout } from "./auth/layout/auth-layout";
+import ChatLayout from "./chat/layout/chat-layout";
+import ChatPage from "./chat/pages/chat-page";
+
+function App() {
+  return (
+    <>
+      {/* <AuthLayout /> */}
+      <ChatLayout>
+        <ChatPage />
+      </ChatLayout>
+    </>
+  );
+}
+
+export default App;
